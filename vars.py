8bf8cbcdb14aa8c1c7545dbd1ea6d6298ae5b756
STARAJ03 <127228123+STARAJ03@users.noreply.github.com>
2025-06-04 19:57:40 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -2,6 +2,6 @@ api_id = 27765349 #change_with_your_Api_id.
 api_hash = "9df1f705c8047ac0d723b29069a1332b" #change_with_your_Api_hash.
 bot_token = "7998916604:AAHCBE5g5fGL0rwqkFEbujQi9-nxadLZGQk" #change_with_your_bot_token.
 auth_users = [1116405290] #change_with_your_user_id.
-#sudo_user = 1713924419 #change_with_your_user_id.
+sudo_user = 1116405290 #change_with_your_user_id.
 #log_channel = -1002154281532 #change_with_your_channel_id.
 #txt_channel = -1002154281532 #change_with_your_channel_id.
