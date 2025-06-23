25ca5b5181a1dd407c647e1078b8fa8e98242fa5
STARAJ03 <127228123+STARAJ03@users.noreply.github.com>
2025-06-23 11:36:45 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,6 +1,6 @@
 api_id = 27765349 #change_with_your_Api_id.
 api_hash = "9df1f705c8047ac0d723b29069a1332b" #change_with_your_Api_hash.
-bot_token = "7998916604:AAHCBE5g5fGL0rwqkFEbujQi9-nxadLZGQk" #change_with_your_bot_token.
+bot_token = "7961586910:AAHyomead8Y8TmujE87R-yPGQzX9WSzMf1E" #change_with_your_bot_token.
 auth_users = [1116405290] #change_with_your_user_id.
 sudo_user = 1116405290 #change_with_your_user_id.
 log_channel = -1116405290 #change_with_your_channel_id.
